6c9ec5a00bed9e52d2638091166790ecd5b8e09c
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2021-07-06 09:33:24 +0530

    After defining methods to insert at any position, at the end, display and count the length.

---
+++ src/singlyLinkedList/SinglyLinkedList.java
@@ -0,0 +1,87 @@
+package singlyLinkedList;
+
+import node.Node;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Abhinav Bhardwaj
+ * Date: 06-07-2021
+ * Time: 09:20
+ */
+
+public class SinglyLinkedList {
+    private Node head;
+
+    public void insertAtEnd( int data) {
+        Node newNode = new Node(data);
+        if(head.getNext() == null) {
+            head.setNext(newNode);
+        }
+        else{
+            Node temp = head;
+            while(temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(newNode);
+        }
+    }
+
+    public void insertAtPosition(int position, int data) {
+        Node newNode = new Node(data);
+        if(position == 1) {
+            newNode.setNext(head);
+            head = newNode;
+        }
+        else{
+            Node previous = head;
+            int count = 1;
+            while(count < (position-1)){
+                previous = previous.getNext();
+                count++;
+            }
+            Node current = previous.getNext();
+            newNode.setNext(current);
+            previous.setNext(newNode);
+        }
+    }
+
+    public void delete(int position){
+        if(position == 1){
+            head = head.getNext();
+        }
+        else {
+            Node previous = head;
+            int count = 1;
+            while(count < position-1){
+                previous = previous.getNext();
+                count++;
+            }
+            Node current = previous.getNext();
+            previous.setNext(current.getNext());
+        }
+    }
+
+
+    public int length() {
+        Node temp = head;
+        int count = 0;
+        if(head == null){
+            return 0;
+        }
+        while(temp != null) {
+            count++;
+            temp = temp.getNext();
+        }
+        return count;
+    }
+
+    public void display() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.getData() + " --> ");
+            temp = temp.getNext();
+        }
+        System.out.print("null");
+    }
+
+}

4dda8251cd748fef162885a378fbf01bbd8ed40d
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2021-07-12 09:51:52 +0530

    Main method to run the entire program with a menu.

---
+++ src/singlyLinkedList/Main.java
@@ -0,0 +1,59 @@
+package singlyLinkedList;
+
+import java.util.Scanner;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Abhinav Bhardwaj
+ * Date: 06-07-2021
+ * Time: 09:31
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SinglyLinkedList sll = new SinglyLinkedList();
+        System.out.print("Insert data of your first node :  ");
+        int val = sc.nextInt();
+        sll.insertAtStart(val);
+        boolean condition = true;
+        int choice, data, position;
+        while (condition) {
+            System.out.println("Press 1 to insert data at the start of the LinkedList");
+            System.out.println("Press 2 to insert data at the end of the LinkedList");
+            System.out.println("Press 3 to insert data at any position in the LinkedList");
+            System.out.println("Press 4 to delete node using its position in the LinkedList");
+            System.out.println("Press 5 to display the LinkedList");
+            System.out.println("Press 6 to exit");
+            System.out.print("Your choice : ");
+            choice = sc.nextInt();
+            if(choice == 6){
+                condition = false;
+            }
+            else{
+                switch (choice) {
+                    case 1:
+                        data = sc.nextInt();
+                        sll.insertAtStart(data);
+                        break;
+                    case 2:
+                        data = sc.nextInt();
+                        sll.insertAtEnd(data);
+                        break;
+                    case 3:
+                        data = sc.nextInt();
+                        position = sc.nextInt();
+                        sll.insertAtPosition(position, data);
+                        break;
+                    case 4:
+                        position = sc.nextInt();
+                        sll.delete(position);
+                        break;
+                    case 5:
+                        sll.display();
+                        break;
+                }
+            }
+        }
+    }
+}

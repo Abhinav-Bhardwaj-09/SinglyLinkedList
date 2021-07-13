2d6c51b9c4dcb8720a0d528426d27272372b35ce
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2021-07-13 09:48:44 +0530

    Debugging

--- src/singlyLinkedList/Main.java
+++ src/singlyLinkedList/Main.java
@@ -19,12 +19,13 @@ public class Main {
         boolean condition = true;
         int choice, data, position;
         while (condition) {
+            System.out.println("\n");
             System.out.println("Press 1 to insert data at the start of the LinkedList");
             System.out.println("Press 2 to insert data at the end of the LinkedList");
             System.out.println("Press 3 to insert data at any position in the LinkedList");
             System.out.println("Press 4 to delete node using its position in the LinkedList");
             System.out.println("Press 5 to display the LinkedList");
-            System.out.println("Press 6 to exit");
+            System.out.println("Press 6 to exit\n");
             System.out.print("Your choice : ");
             choice = sc.nextInt();
             if(choice == 6){
@@ -33,23 +34,45 @@ public class Main {
             else{
                 switch (choice) {
                     case 1:
+                        System.out.print("Data value: ");
                         data = sc.nextInt();
                         sll.insertAtStart(data);
+                        System.out.println();
+                        System.out.println();
                         break;
                     case 2:
+                        System.out.print("Data value: ");
                         data = sc.nextInt();
                         sll.insertAtEnd(data);
                         break;
                     case 3:
+                        System.out.print("Data value: ");
                         data = sc.nextInt();
+                        System.out.println();
+                        System.out.print("Position :");
                         position = sc.nextInt();
+                        while (position > sll.length()) {
+                            System.out.println();
+                            System.out.println("Position should lie inside LinkedList.");
+                            System.out.print("Position :");
+                            position = sc.nextInt();
+                        }
                         sll.insertAtPosition(position, data);
                         break;
                     case 4:
+                        System.out.print("Position :");
                         position = sc.nextInt();
+                        while (position > sll.length()) {
+                            System.out.println();
+                            System.out.println("Position should lie inside LinkedList.");
+                            System.out.print("Position :");
+                            position = sc.nextInt();
+                        }
                         sll.delete(position);
                         break;
                     case 5:
+                        System.out.println("\n");
+                        System.out.print("LinkedList:  ");
                         sll.display();
                         break;
                 }
